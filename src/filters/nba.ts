6b7079f590f8e1cbd3d75fc4ab3a20b30928dcc5
ildar <ildar.tamendarov@gmail.com>
2023-05-19 23:48:51 +0600

    update

--- src/filters/nba.ts
+++ src/filters/nba.ts
@@ -1,30 +1,30 @@
 
 export function hasShotType(ev: any, shotType: string): boolean {
-    if (!ev.payload.event.statistics) {
-        return false;
-    }
-    for (let stat of ev.payload.event.statistics) {
-        if (stat.shot_type === shotType || stat.type === shotType) {
-            return true;
-        }
-    }
+    // if (!ev.payload.event.statistics) {
+    //     return false;
+    // }
+    // for (let stat of ev.payload.event.statistics) {
+    //     if (stat.shot_type === shotType || stat.type === shotType) {
+    //         return true;
+    //     }
+    // }
     return false;
 }
 
 export function hasPlayerName(ev: PayloadEvent, playerName: string): boolean {
-    if (!ev.payload.event.statistics) {
-        return false;
-    }
-    for (let stat of ev.payload.event.statistics) {
-        if (!stat.player) {
-            continue;
-        }
-        if (stat.type === "assist") { // Ignore assists.
-            continue;
-        }
-        if (stat.player.full_name === playerName) {
-            return true;
-        }
-    }
+    // if (!ev.payload.event.statistics) {
+    //     return false;
+    // }
+    // for (let stat of ev.payload.event.statistics) {
+    //     if (!stat.player) {
+    //         continue;
+    //     }
+    //     if (stat.type === "assist") { // Ignore assists.
+    //         continue;
+    //     }
+    //     if (stat.player.full_name === playerName) {
+    //         return true;
+    //     }
+    // }
     return false;
 }
